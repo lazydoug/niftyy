84f12a4587d0c98d84bef4544f58bcb777797993
Douglas <id.doglax@gmail.com>
2024-03-09 22:00:49 +0100

    Spotlight section start

--- src/components/auctions.jsx
+++ src/components/auctions.jsx
@@ -9,9 +9,7 @@ function Auctions() {
       artist: '@randomdash',
       asset: '/auction-1-lr.jpg',
       bid: '8.00 ETH',
-      hrs: '23',
-      min: '59',
-      sec: '59',
+      time: { hrs: '23', min: '59', sec: '59' },
     },
   ]
 
@@ -19,62 +17,71 @@ function Auctions() {
     <section className='md:flex'>
       <div className='flex flex-col gap-10 px-6 py-10 bg-accent-2'>
         <h2 className='text-h1-sm text-neutral-700'>Auctions ending soon</h2>
-        <BtnPrimaryLight className='w-max'>
+        <BtnPrimaryLight className='w-fit'>
           Explore More
-          <span className='bg-neutral-300 text-button-lg rounded-xl px-2 py-0.5 ml-3'>
+          <span className='inline-block bg-neutral-300 rounded-xl px-2 py-0.5 ml-3'>
             {numberOfAuctions}
           </span>
         </BtnPrimaryLight>
       </div>
 
-      <div
-        className='flex flex-col w-full h-[375px] p-6 bg-center bg-cover'
-        style={{
-          backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.85)), url(${auctions[0].asset})`,
-        }}>
-        <ImageCard
-          title={auctions[0].artist}
-          image={auctions[0].avatar}
-          background='bg-opacity-700-75'
+      {auctions.map(auction => (
+        <AuctionCard
+          key={auction.artist}
+          artist={auction.artist}
+          avatar={auction.avatar}
+          asset={auction.asset}
+          bid={auction.bid}
+          duration={auction.time}
         />
-        <div className='mt-auto flex gap-6'>
-          <div className='min-w-max'>
-            <p className='mb-3 text-base-sm text-neutral-100'>Current bid</p>
-            <h4 className='text-h4-sm text-neutral-100'>
-              {auctions[0].bid}
+      ))}
+    </section>
+  )
+}
+
+function AuctionCard({ artist, avatar, asset, bid, duration }) {
+  return (
+    <div
+      className='flex flex-col w-full h-[375px] p-6 bg-center bg-cover'
+      style={{
+        backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.85)), url(${asset})`,
+      }}>
+      <ImageCard title={artist} image={avatar} background='bg-opacity-700-75' />
+      <div className='mt-auto flex gap-6'>
+        <div className='min-w-max'>
+          <p className='mb-3 text-base-sm text-neutral-100'>Current bid</p>
+          <h4 className='text-h4-sm text-neutral-100'>
+            {bid}
+            <span className='block mt-2 text-base-sm text-neutral-200'>
+              {'$24,635.39'}
+            </span>
+          </h4>
+        </div>
+        <div className='w-full'>
+          <p className='mb-3 text-base-sm text-neutral-100'>Auction ends in</p>
+          <div className='flex gap-5'>
+            <h4 className='w-full text-h4-sm text-neutral-100'>
+              {duration.hrs}
               <span className='block mt-2 text-base-sm text-neutral-200'>
-                {'$24,635.39'}
+                hrs
+              </span>
+            </h4>
+            <h4 className='w-full text-h4-sm text-neutral-100'>
+              {duration.min}
+              <span className='block mt-2 text-base-sm text-neutral-200'>
+                min
+              </span>
+            </h4>
+            <h4 className='w-full text-h4-sm text-neutral-100'>
+              {duration.sec}
+              <span className='block mt-2 text-base-sm text-neutral-200'>
+                sec
               </span>
             </h4>
           </div>
-          <div className='w-full'>
-            <p className='mb-3 text-base-sm text-neutral-100'>
-              Auction ends in
-            </p>
-            <div className='flex gap-5'>
-              <h4 className='w-full text-h4-sm text-neutral-100'>
-                {auctions[0].hrs}
-                <span className='block mt-2 text-base-sm text-neutral-200'>
-                  hrs
-                </span>
-              </h4>
-              <h4 className='w-full text-h4-sm text-neutral-100'>
-                {auctions[0].min}
-                <span className='block mt-2 text-base-sm text-neutral-200'>
-                  min
-                </span>
-              </h4>
-              <h4 className='w-full text-h4-sm text-neutral-100'>
-                {auctions[0].sec}
-                <span className='block mt-2 text-base-sm text-neutral-200'>
-                  sec
-                </span>
-              </h4>
-            </div>
-          </div>
         </div>
       </div>
-    </section>
+    </div>
   )
 }
 
